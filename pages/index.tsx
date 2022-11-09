ab850f0655a0ad7c9a2e201ed22498c37311efa4
rahathosen <70264343+rahathosen@users.noreply.github.com>
2022-11-09 23:57:24 +0600

    add mobile nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,10 @@ import Card from './card'
 export default function Home() {
   return (
    <div>
+      <div>
+   
       <Nav/>
+      </div>
       <div className='p-10'>
         <Card/>
         </div>  
--- pages/nav.tsx
+++ pages/nav.tsx
@@ -1,5 +1,8 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
+import { Transition } from "@headlessui/react";
+
+
 
 
 type MenuItemProps = {
@@ -14,28 +17,39 @@ type SubItemProps = {
 
 const Underline = () => (
   <motion.div
-    className="absolute bottom-5 left-0 right-0 h-1 bg-gradient-to-r from-stone-400 via-neutral-200 to-stone-400"
+    className="absolute bottom-5 left-0 right-0 h-1 bg-gradient-to-r from-neutral-400 via-neutral-200 to-neutral-400"
     layoutId="underline"
     layout
   ></motion.div>
 );
 
-const NiceMenu = () => {
+function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="w-screen p-5 font-bold">
+    <div>
+      <nav className="">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <div className="hidden md:block">
+
+                
+            {/* large nav */}
+           
+            <div className="w-screen p-5 font-bold  hidden md:block">
       <motion.div className=" p-5 flex justify-center ">
         <MenuItem text={"Home"}>
-          <SubItem title="Product" text="e-commerce" />
+          <SubItem title="Product" text="Modern e-commerce" />
           <SubItem title="Blog" text="Latest posts" />
-          <SubItem title="Contact" text="Get in touch" />
+          
         </MenuItem>
         <MenuItem text={"Tech"}>
-          <SubItem title="Product" text="e-commerce" />
+          <SubItem title="Product" text="Modern e-commerce" />
           <SubItem title="Blog" text="Latest posts" />
           <SubItem title="Contact" text="Get in touch" />
         </MenuItem>
         <MenuItem text={"Travel"}>
-          <SubItem title="Product" text="e-commerce" />
+          <SubItem title="Product" text="Modern e-commerce" />
           <SubItem title="Blog" text="Latest posts" />
           <SubItem title="Contact" text="Get in touch" />
         </MenuItem>
@@ -46,84 +60,193 @@ const NiceMenu = () => {
        
       </motion.div>
     </div>
+   {/* large nav*/}
+         </div>
+            </div>
+            <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex-shrink-0 flex items-center">
+                  <div
+                    className="block lg:hidden h-8 w-auto text-lg font-bold">
+                    Next Blog
+                  </div>
+                  <div
+                    className="hidden lg:block h-8 w-auto text-lg font-bold" >Next Blog
+                    </div>   
+                </div>
+                </div>
+            <div className="-mr-2 flex md:hidden">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                type="button"
+                className="bg-gray-200 inline-flex items-center justify-center p-2 
+                rounded-md text-gray-800 hover:text-neutral-800 hover:bg-gray-300 
+                focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-200
+                 focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open main menu</span>
+                {!isOpen ? (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <Transition
+          show={isOpen}
+          enter="transition ease-out duration-100 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-in duration-75 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          {(ref) => (
+            <div className="md:hidden" id="mobile-menu">
+              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                <a
+                  href="#"
+                  className="text-neutral-800 hover:bg-neutral-400 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Home
+                </a>
+                <a
+                  href="#"
+                  className="text-neutral-800 hover:bg-neutral-400 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Tech
+                </a>
+
+                <a
+                  href="#"
+                  className="text-neutral-800 hover:bg-neutral-400 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Travel
+                </a>
+
+                <a
+                  href="#"
+                  className="text-neutral-800 hover:bg-neutral-400 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  About us
+                </a>
+              </div>
+            </div>
+          )}
+        </Transition>
+      </nav>
+    </div>
   );
-};
+}
+
 
 const MenuItemVariants = {
-  hidden: {
-    opacity: 0,
-  },
-  visible: {
-    x: 0,
-    opacity: 1,
-
-    transition: {
-      staggerChildren: 0.05,
+    hidden: {
+      opacity: 0,
     },
-  },
-};
-
-const  MenuItem: React.FC<MenuItemProps> =  ({ text, children, ...props }) => {
-  const [isBeingHovered, setIsBeingHovered] = useState(false);
-
-  return (
-    <motion.div
-      className="px-10 relative cursor-pointer z-10"
-      onHoverStart={() => setIsBeingHovered(true)}
-      onHoverEnd={() => setIsBeingHovered(false)}
-    >
-      <span className="relative">
-        {text}
-        {isBeingHovered && <Underline />}
-      </span>
-      {isBeingHovered && (
-        <div className="">
-          <motion.div
-            {...props}
-            layoutId="menu"
-            className="absolute border rounded-lg border-1 shadow-lg py-8 px-8 bg-white rounded-box "
-            variants={MenuItemVariants}
-            initial="hidden"
-            animate="visible"
-          >
-            {children}
-          </motion.div>
+    visible: {
+      x: 0,
+      opacity: 1,
+  
+      transition: {
+        staggerChildren: 0.05,
+      },
+    },
+  };
+  
+  const  MenuItem: React.FC<MenuItemProps> =  ({ text, children, ...props }) => {
+    const [isBeingHovered, setIsBeingHovered] = useState(false);
+  
+    return (
+      <motion.div
+        className="px-10 relative cursor-pointer z-10"
+        onHoverStart={() => setIsBeingHovered(true)}
+        onHoverEnd={() => setIsBeingHovered(false)}
+      >
+        <span className="relative">
+          {text}
+          {isBeingHovered && <Underline />}
+        </span>
+        {isBeingHovered && (
+          <div className="">
+            <motion.div
+              {...props}
+              layoutId="menu"
+              className="absolute border rounded-lg border-1 shadow-lg py-8 px-8 bg-white rounded-box "
+              variants={MenuItemVariants}
+              initial="hidden"
+              animate="visible"
+            >
+              {children}
+            </motion.div>
+          </div>
+        )}
+      </motion.div>
+    );
+  };
+  
+  const SubItemVariants = {
+    hidden: {
+      x: -20,
+      opacity: 0,
+    },
+    visible: {
+      x: 0,
+      opacity: 1,
+    },
+  };
+  
+  const SubItem: React.FC<SubItemProps> = ({ title, text }) => {
+    return (
+      <motion.div
+        className="my-2 group cursor-pointer min-w-max"
+        layout
+        variants={SubItemVariants}
+      >
+        <div className="flex items-center gap-4">
+          <div className="py-2">
+            <p className="font-bold text-gray-800 group-hover:text-blue-900 text-md">
+              {title}
+            </p>
+            <span className="font-bold text-gray-400 group-hover:text-blue-400 text-sm">
+              {text}
+            </span>
+          </div>
         </div>
-      )}
-    </motion.div>
-  );
-};
+      </motion.div>
+    );
+  };
 
-const SubItemVariants = {
-  hidden: {
-    x: -20,
-    opacity: 0,
-  },
-  visible: {
-    x: 0,
-    opacity: 1,
-  },
-};
-
-const SubItem: React.FC<SubItemProps> = ({ title, text }) => {
-  return (
-    <motion.div
-      className="my-2 group cursor-pointer min-w-max"
-      layout
-      variants={SubItemVariants}
-    >
-      <div className="flex items-center gap-4">
-        <div className="py-2">
-          <p className="font-bold text-gray-800 group-hover:text-blue-900 text-md">
-            {title}
-          </p>
-          <span className="font-bold text-gray-400 group-hover:text-blue-400 text-sm">
-            {text}
-          </span>
-        </div>
-      </div>
-    </motion.div>
-  );
-};
-
-export default NiceMenu;
+export default Nav;
